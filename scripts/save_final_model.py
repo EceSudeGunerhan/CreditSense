d02bf09bcc4826f635bb530a22bf4fb8fa9623ff
EceSudeGunerhan <149162447+EceSudeGunerhan@users.noreply.github.com>
2025-05-29 23:09:03 +0300

    Update save_final_model.py

--- scripts/save_final_model.py
+++ scripts/save_final_model.py
@@ -5,10 +5,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 
-'''
-burada fastapide kullanacağımız model kaydını yaptık. standadScaler ve one-hot encoding ile.
-'''
-
 DATA_PATH = "data/processed/cleaned_data.csv"
 df = pd.read_csv(DATA_PATH)
 
@@ -16,11 +12,9 @@ target_col = "BAD"
 X = df.drop(columns=[target_col])
 y = df[target_col]
 
-# One-hot encoding
 cat_cols = X.select_dtypes(include=["object"]).columns.tolist()
 X_encoded = pd.get_dummies(X, columns=cat_cols, drop_first=True)
 
-# StandardScaler
 num_cols = X_encoded.select_dtypes(include=["int64", "float64"]).columns.tolist()
 scaler = StandardScaler()
 X_encoded[num_cols] = scaler.fit_transform(X_encoded[num_cols])
@@ -32,12 +26,10 @@ X_train, X_test, y_train, y_test = train_test_split(
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
 
-# Model ve scaler kaydet
 os.makedirs("models", exist_ok=True)
 joblib.dump(model, "models/final_model.pkl")
 joblib.dump(scaler, "models/scaler.pkl")
 
-# Kullanılan encoding sütunlarını da kaydettik (FastAPI'de uyum için)
 joblib.dump(X_encoded.columns.tolist(), "models/feature_columns.pkl")
 
 print("Model ve scaler başarıyla kaydedildi.")
