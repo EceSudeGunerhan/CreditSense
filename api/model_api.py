a44e3021bda1abfba89dec64bd87480f3eaa372f
EceSudeGunerhan <149162447+EceSudeGunerhan@users.noreply.github.com>
2025-05-29 23:10:58 +0300

    Update model_api.py

--- api/model_api.py
+++ api/model_api.py
@@ -1,31 +1,24 @@
 import joblib
 import pandas as pd
 
-'''
-FastAPI uygulamasında tahmin yapmadan önce veriyi işleme (preprocessing) ve tahmin üretme (inference) işlemlerini yaptım.
-'''
-
 model = joblib.load("models/final_model.pkl")
 scaler = joblib.load("models/scaler.pkl")
 feature_columns = joblib.load("models/feature_columns.pkl")
 
 def preprocess_input(data: dict) -> pd.DataFrame:
-    # Gelen veriyi DataFrame'e çevir
+    
     df = pd.DataFrame([data])
 
-    # Kategorik sütunları one-hot encode et
     cat_cols = df.select_dtypes(include="object").columns.tolist()
     df_encoded = pd.get_dummies(df, columns=cat_cols, drop_first=True)
 
-    # Eksik kalan sütunları (modelin eğitildiği sütunlara göre) ekle
     for col in feature_columns:
         if col not in df_encoded.columns:
             df_encoded[col] = 0
 
-    # Sıralamayı modeldeki sıraya göre düzenle
     df_encoded = df_encoded[feature_columns]
 
-    scaled_cols = scaler.feature_names_in_  # Sadece scaler'ın eğitildiği sütunlara uygula
+    scaled_cols = scaler.feature_names_in_  
     df_encoded[scaled_cols] = scaler.transform(df_encoded[scaled_cols])
 
     return df_encoded
